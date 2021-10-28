868c84634868dd47489cc6ac49a8b5c7c549b008
Tomas Green <tomas.green@gmail.com>
2021-10-28 12:04:10 +0200

    Updates to latest dependency versions

--- Package.swift
+++ Package.swift
@@ -12,8 +12,8 @@ let package = Package(
             targets: ["Assistant"]),
     ],
     dependencies: [
-        .package(name: "TTS", url: "https://github.com/helsingborg-stad/spm-tts.git", from: "0.1.3"),
-        .package(name: "STT", url: "https://github.com/helsingborg-stad/spm-stt.git", from: "0.2.0"),
+        .package(name: "TTS", url: "https://github.com/helsingborg-stad/spm-tts.git", from: "0.1.4"),
+        .package(name: "STT", url: "https://github.com/helsingborg-stad/spm-stt.git", from: "0.2.1"),
         .package(name: "TextTranslator", url: "https://github.com/helsingborg-stad/spm-text-translator", from: "0.2.0"),
         .package(name: "Dragoman", url: "https://github.com/helsingborg-stad/spm-dragoman.git", from: "0.1.4")
     ],
